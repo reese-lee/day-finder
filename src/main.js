811bda248558d2076558a36a151afd15ed72385c
kaya jepson and Reese Lee <reese@>
2019-05-29 11:39:54 -0700

    broken math

--- src/main.js
+++ src/main.js
@@ -4,10 +4,20 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-$(function() {
-  let result = dayFinder();
-    $(".day").text(result);
-    $("#result").show();
+// $(function() {
+//   let result = dayFinder();
+//     // $(".day").text(result);
+//     $(".result").text(result);
+//
+//   // });
+// });
 
-  // });
+
+$(document).ready(function() {
+  $("form#dayDate").submit(function(event) {
+    event.preventDefault();
+    let results = dayFinder();
+    $("#result").text(results).show();
+    console.log(results);
+  });
 });
